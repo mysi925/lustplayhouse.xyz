b2672bc8e828c61c6b98bb39ab288b8e2a7b957c
mysi925 <m4yasi@gmail.com>
2026-06-24 18:33:59 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "./static",
-  base: "./",
+  publicDir: "public",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
